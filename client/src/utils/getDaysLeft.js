2e9448fa175f5c72cda6a6d75314df266db672fb
daniyalareeb <daniyalareeb123@gmail.com>
2026-04-19 19:45:42 +0100

    fix: milliseconds vs seconds deadline bug

--- client/src/utils/getDaysLeft.js
+++ client/src/utils/getDaysLeft.js
@@ -1,6 +1,7 @@
 export default (deadline) => {
+    const deadlineMs = deadline * 1000;
     const currentTime = new Date().getTime();
-    const remainingTime = deadline - currentTime;
+    const remainingTime = deadlineMs - currentTime;
     const remainingDays = remainingTime / (1000 * 3600 * 24);
     return Math.ceil(remainingDays);
 }
